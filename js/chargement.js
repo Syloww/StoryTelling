417934b8529dc547c7eb8ba55edcb477deeefdd0
sylow <glenacsalim@gmail.com>
2024-12-03 16:04:53 +0100

    Update chargement.js

--- js/chargement.js
+++ js/chargement.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const loadingElement = document.getElementById("chargement");
 
     if (loadingElement) {
-        loadingElement.style.display = "none"; // Cache l'élément de chargement
-    }
-    
-
+        setTimeout(() => {
+            loadingElement.style.display = "none"; // Cache l'élément de chargement après 1 seconde
+        }, 500); // 1000 millisecondes = 1 seconde
+    }    
 });
