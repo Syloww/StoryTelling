bd31f6fa039cbd648e07b250af81f760970e1937
sylow <glenacsalim@gmail.com>
2024-12-03 15:59:17 +0100

    chargement

---
+++ js/chargement.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loadingElement = document.getElementById("chargement");
+
+    if (loadingElement) {
+        loadingElement.style.display = "none"; // Cache l'élément de chargement
+    }
+    
+
+});
